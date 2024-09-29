b8cf6985b12b9251b17e1ef3d81d00809f096ed6
Subhasish Samaddar <subhasish.samaddar@techcrista.com>
2024-09-29 14:17:06 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Location from "./component/Location";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Select Location</h1>
+      <Location />
+    </div>
+  );
+}
+
+export default App;
